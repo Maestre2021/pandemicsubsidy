c9aa859784e15b15a147e192d6f84b0f09bb0443
roli anne <rolianne.maestre@gsfe.tupcavite.edu.ph>
2021-04-04 18:24:28 +0800

    This is my new commit

--- projectroli/urls.py
+++ projectroli/urls.py
@@ -1,3 +1,9 @@
+
+from django.conf.urls import url
+from CTList import views 
+
+urlpatterns = [
+	url(r'^$' , views.MainPage, name='mainpage'),
 """projectroli URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
